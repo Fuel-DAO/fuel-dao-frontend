0bd944360037b91d763d99a3ccf5553958cda1b5
harshfuel <harsh@fueldao.io>
2024-05-20 13:48:13 +0530

    Add canister IDs

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ const prod = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
 	define: {
+		'process.env.CANISTER_ID_ASSET': JSON.stringify('6kdae-saaaa-aaaam-acnuq-cai'),
+		'process.env.CANISTER_ID_ASSET_PROXY': JSON.stringify('6ncgq-7yaaa-aaaam-acnua-cai'),
+		'process.env.CANISTER_ID_PROVISION': JSON.stringify('7amc6-qqaaa-aaaam-acntq-cai'),
+		'process.env.INTERNET_IDENTITY_CANISTER_ID': JSON.stringify('bw4dl-smaaa-aaaaa-qaacq-cai'),
 		'import.meta.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'dev'),
 		'import.meta.env.DFX_PORT': JSON.stringify(DFX_PORT),
 		'process.env.DFX_NETWORK': JSON.stringify(prod ? 'ic' : 'local')
@@ -15,7 +19,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: `http://0.0.0.0:${DFX_PORT}`,
+				target: `http://0.0.0.0:${DFX_PORT}`
 				// This proxies all http requests made to /api to our running dfx instance
 			}
 		},
