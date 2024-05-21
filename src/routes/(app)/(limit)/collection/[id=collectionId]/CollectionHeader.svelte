5ddc5f503af22e11b92d519026606a502e979716
harshfuel <harsh@fueldao.io>
2024-05-21 16:12:57 +0530

    Fix

--- src/routes/(app)/(limit)/collection/[id=collectionId]/CollectionHeader.svelte
+++ src/routes/(app)/(limit)/collection/[id=collectionId]/CollectionHeader.svelte
@@ -19,11 +19,11 @@
 </script>
 
 <div class="flex flex-col grow gap-4">
-	<div class="flex flex-col sm:flex-row gap-4 lg:justify-between">
+	<div class="flex flex-col sm:flex-row gap-4 lg:justify-between lg:items-center">
 		<div class="flex flex-col lg:flex-row gap-8 items-start lg:items-center">
 			<div class="text-2xl lg:text-5xl font-bold">{metadata.name}</div>
 			<div
-				class="py-2 px-4 text-xs translate-y-1 bg-black rounded-full text-white font-light flex h-min items-center justify-center"
+				class="py-2 px-4 text-xs bg-black rounded-full text-white font-light flex h-min items-center justify-center"
 			>
 				Live
 			</div>
