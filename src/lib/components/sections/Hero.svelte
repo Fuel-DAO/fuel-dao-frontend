a7d321bce82c07833ac16f9cbd594fa1d5a36e53
shreyfuel <shreyansh@fueldao.io>
2024-05-25 20:49:58 +0530

    added invest now button

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import hero from './hero.webp';
+	import Button from '../button/Button.svelte';
+import hero from './hero.webp';
 </script>
 
 <section id="hero" class="h-dvh w-full flex items-start justify-start relative">
-	<div class="xl:px-32 z-[2] px-8 text-4xl xl:text-8xl leading-normal font-semibold pt-48">
-		Driving shared <br /> mobility forward, <br /> clean & decentralized
-	</div>
+  <div class="xl:px-32 z-[2] px-8 pt-48">
+    <div class="text-4xl xl:text-6xl font-semibold mb-8">
+      Driving Shared <br /> Mobility Forward, <br /> Clean & Decentralized
+    </div>
+    <Button
+      href="/collections"
+      class="xl:text-xl xl:px-6 xl:py-3">Invest Now</Button>
+  </div>
 	<div class="absolute inset-0 z-[1] overflow-hidden">
 		<div
 			class="h-full w-full lg:bg-cover bg-contain bg-no-repeat lg:bg-right bg-bottom scale-150 lg:scale-100 origin-bottom lg:origin-center"
