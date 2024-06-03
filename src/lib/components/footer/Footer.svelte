7890d803071e13f7d30217f9b480ceef3a2e0d58
shreyfuel <shreyansh@fueldao.io>
2024-06-03 09:23:38 +0530

    updated contact email

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -13,7 +13,7 @@
 		<div
 			class="lg:w-1/3 w-full flex justify-center items-start flex-col border-b lg:border-r self-stretch p-8 border-white"
 		>
-			<a href="mailto:kshitij@fueldao.io">kshitij@fueldao.io</a>
+			<a href="mailto:info@fueldao.io">info@fueldao.io</a>
 		</div>
 		<div
 			class="lg:w-1/3 w-full flex justify-center items-start self-stretch p-8 flex-col lg:border-r border-white"
